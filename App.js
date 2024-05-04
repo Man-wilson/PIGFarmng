aa355694f5bee7b06b83a62895622d5b439f876c
Mandela <mandelawilsonn@gmail.com>
2024-05-04 17:24:42 +0200

    used expo constants

--- App.js
+++ App.js
@@ -27,18 +27,18 @@ export default function App() {
   });
 
   async function onFetchUpdateAsync() {
+    // Check if running inside Expo Go
     if (Constants.appOwnership === "expo") {
       return;
     }
+
     try {
       const update = await Updates.checkForUpdateAsync();
-
       if (update.isAvailable) {
         await Updates.fetchUpdateAsync();
         await Updates.reloadAsync();
       }
     } catch (error) {
-      // You can also add an alert() to see the error message in case of an error when fetching updates.
       alert(`Error fetching latest Expo update: ${error}`);
     }
   }
